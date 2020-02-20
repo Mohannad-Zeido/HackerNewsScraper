98bb2c309bdbc696404810ea6c0a23242dc1359c
Mohannad Zeido <mohannad.zeido@gmail.com>
2020-02-20 09:16:20 +0000

    Initial playing around to figure out structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Mohannad-Zeido/HackerNewsScraper
 
 go 1.13
+
+require github.com/gocolly/colly/v2 v2.0.1
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
 func main() {
+	c := colly.NewCollector()
 
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		if e.Attr("class") == "athing"{
+			fmt.Println(e)
+		}
+		//link := e.Attr("href")
+		//// Print link
+		//fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		//// Visit link found on page
+		//// Only those links are visited which are in AllowedDomains
+		//c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnHTML("td", func(e *colly.HTMLElement) {
+		if e.Attr("class") == "subtext"{
+			fmt.Println(e)
+		}
+
+		if e.Attr("class") == "morelink"{
+			fmt.Println(e)
+		}
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		if e.Attr("class") == "morelink"{
+			fmt.Println(e.Attr("href"))
+		}
+	})
+
+	c.Visit("https://news.ycombinator.com/")
 }
