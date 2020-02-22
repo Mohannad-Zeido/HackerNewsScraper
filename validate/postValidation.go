b8d0b4c0399006b4973627dddcb9c419c21bf9fa
Mohannad-Zeido <mohannad.zeido@gmail.com>
2020-02-22 17:47:33 +0000

    Add regex to remove non-numeric and add empty string  check

--- validate/postValidation.go
+++ validate/postValidation.go
@@ -1,15 +1,26 @@
 package validate
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+var (
+	nonNumbersRegex, _ = regexp.Compile("\\D")
+)
 
 func IsValidText(text string) bool {
-	if text == "" || len(text) > 256 {
+	if isStringEmpty(text) || len(text) > 256 {
 		return false
 	}
 	return true
 }
 
 func IsValidUri(uri string) bool {
+	if isStringEmpty(uri) {
+		return false
+	}
 	resp, err := http.Get(uri)
 	if err != nil || resp.StatusCode != http.StatusOK {
 		return false
@@ -17,8 +28,22 @@ func IsValidUri(uri string) bool {
 	return true
 }
 
-func IsValidNumber(number int) bool {
-	if number < 0 {
+func IsValidNumber(stringToCheck string) bool {
+	if isStringEmpty(stringToCheck) {
+		return false
+	}
+	numberToCheck, err := strconv.Atoi(nonNumbersRegex.ReplaceAllString(stringToCheck, ""))
+	if err != nil {
+		return false
+	}
+	if numberToCheck < 0 {
+		return false
+	}
+	return true
+}
+
+func isStringEmpty(s string) bool {
+	if s == "" {
 		return false
 	}
 	return true
