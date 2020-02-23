75b8498bee86be1ded1a528e0b963d26b617e3c4
Mohannad-Zeido <mohannad.zeido@gmail.com>
2020-02-23 18:49:27 +0000

    update error text

--- types/errorText.go
+++ types/errorText.go
@@ -1,22 +1,9 @@
 package types
 
 const (
-	ErrParsingHTML                 = "error scraping the web page"
-	ErrGettingNextPost             = "error getting the next post"
-	ErrGettingPostDetailsNode      = "error getting post detains node"
-	ErrGettingRankNode             = "error getting rank node"
-	ErrGettingUriParentNode        = "error getting node that contains the uri node"
-	ErrGettingUriNode              = "error getting uri node"
-	ErrGettingUriGeneralInfoNode   = "error getting uri general info node"
-	ErrGettingTitleParentNode      = "error getting node that contains the title node"
-	ErrGettingTitleNode            = "error getting title node"
-	ErrGettingTitleGeneralInfoNode = "error getting title general info node"
-	ErrGettingPointsNode           = "error getting score node"
-	ErrGettingAuthorNode           = "error getting author node"
-	ErrGettingAuthorDetailsNode    = "error getting author details node"
-	ErrGettingCommentsNode         = "error getting comments node"
-	ErrGettingCommentsDetailsNode  = "error getting comments details node"
-	ErrGettingPostsTableNode       = "parsing error unable to find posts table node"
-	ErrGettingPostsTbodyNode       = "parsing error unable to find tbody node for the post table"
-	ErrNoPostsOnPage               = "parsing error no posts in table"
+	ErrGettingNextPost        = "parsing error getting the next post"
+	ErrGettingPostDetailsNode = "parsing error getting post details node"
+	ErrGettingPostsTableNode  = "parsing error unable to find posts table node"
+	ErrGettingPostsTbodyNode  = "parsing error unable to find tbody node for the post table"
+	ErrNoPostsOnPage          = "parsing error no posts in table"
 )
