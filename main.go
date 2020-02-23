882ad0714ba08934d9d53bad23d8fa1d280c1272
Mohannad-Zeido <mohannad.zeido@gmail.com>
2020-02-23 16:51:29 +0000

    refactor in main.go

--- main.go
+++ main.go
@@ -16,22 +16,31 @@ func main() {
 	flag.IntVar(&numberOfPosts, "p", -1, "how many posts to print. A positive integer <= 100  (shorthand)")
 
 	flag.Parse()
+
 	if numberOfPosts < 0 || numberOfPosts > 100 {
 		fmt.Println("please input a valid number of posts to get (A positive integer <= 100)")
 		return
 	}
+
 	posts, err := post.GetPosts(numberOfPosts, types.RunState{TestState: false})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print("number of post gotten ")
-	fmt.Println(len(posts))
-	pos, err := json.MarshalIndent(posts, "", "\t")
+
+	postsJson, err := parsePostsToJSON(posts)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(pos))
+	fmt.Println(postsJson)
 
+}
+
+func parsePostsToJSON(posts []types.Post) (string, error) {
+	postsJSON, err := json.MarshalIndent(posts, "", "\t")
+	if err != nil {
+		return "", err
+	}
+	return string(postsJSON), nil
 }
