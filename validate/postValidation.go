0124de8211fb65b8af8176f46575fe44fc39efae
Mohannad-Zeido <mohannad.zeido@gmail.com>
2020-02-23 13:19:01 +0000

    remove unnecessary debug logging

--- validate/postValidation.go
+++ validate/postValidation.go
@@ -1,13 +1,11 @@
 package validate
 
 import (
-	"fmt"
 	"net/url"
 )
 
 func IsValidText(text string) bool {
 	if text == "" || len(text) > 256 {
-		fmt.Println("Title '" + text + "' failed validation")
 		return false
 	}
 	return true
@@ -16,7 +14,6 @@ func IsValidText(text string) bool {
 func IsValidUri(uri string) bool {
 	_, err := url.ParseRequestURI(uri)
 	if err != nil {
-		fmt.Println("uri '" + uri + "' failed validation")
 		return false
 	}
 	return true
@@ -25,7 +22,6 @@ func IsValidUri(uri string) bool {
 
 func IsValidNumber(numberToCheck int) bool {
 	if numberToCheck < 0 {
-		fmt.Println("number '" + string(numberToCheck) + "' failed validation")
 		return false
 	}
 	return true
